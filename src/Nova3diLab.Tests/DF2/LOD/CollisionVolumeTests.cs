ffe8b8dceafaedff79ec5e2cbf499146e6867e73
Tyler Daniels <seesharpcode@gmail.com>
2020-02-09 00:23:38 -0600

    adding collision volume

---
+++ src/Nova3diLab.Tests/DF2/LOD/CollisionVolumeTests.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using Nova3diLab.Model.Lod;
+using Xunit;
+
+namespace Nova3diLab.Tests.DF2.LOD
+{
+    public class CollisionVolumeTests
+    {
+        public static CollisionVolume CollisionVolume => new CollisionVolume
+        {
+            VolumeType = CollisionVolumeType.Normal,
+            BoundingSphereRadius = .866025403784439,
+            BoundingCircleRadius = .707106781186548,
+            XMin = 0,
+            XMax = 1,
+            YMin = 0,
+            YMax = 1,
+            ZMin = 0,
+            ZMax = 1,
+            CollisionPlaneCount = 6,
+        };
+        
+        [Fact]
+        public void SerializeTest()
+        {
+            var expected = File.ReadAllBytes("Resources/collision-volume.3di");
+            Assert.Equal(expected, TestUtils.SerializeToBytes(CollisionVolume));
+        }
+    }
+}
--- src/Nova3diLab/DF2/LOD/CollisionVolume.cs
+++ src/Nova3diLab/DF2/LOD/CollisionVolume.cs
@@ -4,25 +4,26 @@ using System.IO;
 
 namespace Nova3diLab.Model.Lod
 {
-    // TODO finish structure and test
+    // TODO test
+    public enum CollisionVolumeType
+    {
+        Normal = 1,
+        Climbable = 4,
+        Armory = 6,
+        Attachable = 7
+    }
+
     public class CollisionVolume : IModelSerializable
     {
-        public enum CollisionVolumeType
-        {
-            Normal = 1,
-            Climbable = 4,
-            Armory = 6,
-            Attachable = 7
-        }
-
         public CollisionVolumeType VolumeType { get; set; }
         public double XMedian => (XMin + XMax) / 2;
         public double YMedian => (YMin + YMax) / 2;
         public double ZMedian => (ZMin + ZMax) / 2;
         public double BoundingSphereRadius { get; set; }
-        public double XTotal { get; set; }
-        public double YTotal { get; set; }
-        public double ZTotal { get; set; }
+        public double BoundingCircleRadius { get; set; }
+        public double HalfLength => (XMax - XMin) / 2;
+        public double HalfWidth => (YMax - YMin) / 2;
+        public double HalfHeight => (ZMax - ZMin) / 2;
         public double XMin { get; set; }
         public double XMax { get; set; }
         public double YMin { get; set; }
@@ -39,7 +40,20 @@ namespace Nova3diLab.Model.Lod
             writer.WriteFixedPoint(YMedian);
             writer.WriteFixedPoint(ZMedian);
             writer.WriteFixedPoint(BoundingSphereRadius);
-            writer.WriteFixedPoint(BoundingSphereRadius);
+            writer.WriteFixedPoint(BoundingCircleRadius);
+            writer.WriteFixedPoint(BoundingCircleRadius);
+            writer.WriteFixedPoint(HalfLength);
+            writer.WriteFixedPoint(HalfWidth);
+            writer.WriteFixedPoint(HalfHeight);
+            writer.Write(0);
+            writer.WriteFixedPoint(XMin);
+            writer.WriteFixedPoint(XMax);
+            writer.WriteFixedPoint(YMin);
+            writer.WriteFixedPoint(YMax);
+            writer.WriteFixedPoint(ZMin);
+            writer.WriteFixedPoint(ZMax);
+            writer.Write(CollisionPlaneCount);
+            writer.Write(0);
         }
 
         public void Deserialize(BinaryReader reader)
