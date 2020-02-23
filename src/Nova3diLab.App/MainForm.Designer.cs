a21104c6e655fb24c1e863d8b3d32d42402763c7
Tyler Daniels <seesharpcode@gmail.com>
2020-02-23 17:32:05 -0600

    adding app project

---
+++ src/Nova3diLab.App/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Nova3diLab.App
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.loadMqoButton = new System.Windows.Forms.Button();
+            this.textureDataGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextureName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Width = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Height = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IsLightOn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.textureDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadMqoButton
+            // 
+            this.loadMqoButton.AutoSize = true;
+            this.loadMqoButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.loadMqoButton.Location = new System.Drawing.Point(12, 15);
+            this.loadMqoButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.loadMqoButton.Name = "loadMqoButton";
+            this.loadMqoButton.Size = new System.Drawing.Size(91, 27);
+            this.loadMqoButton.TabIndex = 0;
+            this.loadMqoButton.Text = "Import .mqo";
+            this.loadMqoButton.UseVisualStyleBackColor = true;
+            this.loadMqoButton.Click += new System.EventHandler(this.loadMqoButton_Click);
+            // 
+            // textureDataGrid
+            // 
+            this.textureDataGrid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.textureDataGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textureDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.textureDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TextureName,
+            this.Width,
+            this.Height,
+            this.IsLightOn});
+            this.textureDataGrid.Location = new System.Drawing.Point(12, 90);
+            this.textureDataGrid.Name = "textureDataGrid";
+            this.textureDataGrid.RowHeadersVisible = false;
+            this.textureDataGrid.Size = new System.Drawing.Size(457, 159);
+            this.textureDataGrid.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Textures";
+            // 
+            // TextureName
+            // 
+            this.TextureName.HeaderText = "Name";
+            this.TextureName.Name = "TextureName";
+            this.TextureName.Width = 108;
+            // 
+            // Width
+            // 
+            this.Width.HeaderText = "Width";
+            this.Width.Name = "Width";
+            this.Width.Width = 109;
+            // 
+            // Height
+            // 
+            this.Height.HeaderText = "Height";
+            this.Height.Name = "Height";
+            this.Height.Width = 108;
+            // 
+            // IsLightOn
+            // 
+            this.IsLightOn.HeaderText = "Light On?";
+            this.IsLightOn.Name = "IsLightOn";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 524);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textureDataGrid);
+            this.Controls.Add(this.loadMqoButton);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "Nova 3di Lab";
+            ((System.ComponentModel.ISupportInitialize)(this.textureDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadMqoButton;
+        private System.Windows.Forms.DataGridView textureDataGrid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TextureName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Width;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Height;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn IsLightOn;
+    }
+}
+
---
+++ src/Nova3diLab.App/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using Nova3diLab.Df2;
+using Nova3diLab.Mqo;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Nova3diLab.App
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loadMqoButton_Click(object sender, EventArgs e)
+        {
+            using (var fileDialog = new OpenFileDialog { Filter = "Metasequoia Files|*.mqo", Title = "Select .mqo file" })
+            {
+                var result = fileDialog.ShowDialog();       
+                if (result == DialogResult.OK)
+                {
+                    var mqoModel = MqoModel.Load(fileDialog.FileName);
+                    MqoTo3diConverter.Convert(fileDialog.SafeFileName,mqoModel, null);
+                }
+            }
+        }
+    }
+}
